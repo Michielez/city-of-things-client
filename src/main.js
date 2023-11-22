29e0d4a75bab05bf0d40ed1bc071f33d4bef79c7
niels.soete <niels.soete@student.howest.be>
2023-11-22 08:44:39 +0100

    restructure dashboard view

--- src/main.js
+++ src/main.js
@@ -1,35 +1,12 @@
-import { createApp } from 'vue'
-import './style.css'
 import './reset.css'
+import './style.css'
+
+import { createApp } from 'vue'
 import App from './App.vue'
-
-import { createRouter, createWebHistory } from 'vue-router'
-
-import Home from './components/HelloWorld.vue'
-import LagoZwembad from './components/LagoZwembad.vue'
-import FlandersMake from './components/FlandersMake.vue'
-import Grid from './components/Grid.vue'
-import Dashboard from './components/Dashboard.vue'
-import DePenta from './components/DePenta.vue'
-
-const router = createRouter({
-  history: createWebHistory(),
-  routes: [
-    { path: '/', component: Dashboard },
-    { path: '/grid', component: Grid },
-    { path: '/lago-zwembad', component: LagoZwembad},
-    { path: '/flanders-make', component: FlandersMake},
-    { path: '/magdalena-zwembad', component: Home },
-    { path: '/de-penta', component: DePenta },
-    { path: '/kwe-a', component: Home },
-    { path: '/pti', component: Home },
-    { path: '/kubes', component: Home },
-    { path: '/veg-i-tec', component: Home },
-    { path: '/hangar-k', component: Home },
-  ],
-})
+import router from './router'
 
 const app = createApp(App); 
+
 app.use(router);
 
 app.mount('#app');
---
+++ src/views/DashboardView.vue
@@ -0,0 +1,31 @@
+<template>
+    <main>
+        <OnsDoel></OnsDoel>
+        <Verbruik></Verbruik>
+        <ExtraInfo></ExtraInfo>
+    </main>
+</template>
+
+<script>
+import OnsDoel from '../components/dashboard_components/OnsDoel.vue';
+import Verbruik from '../components/dashboard_components/Verbruik.vue';
+import ExtraInfo from '../components/dashboard_components/ExtraInfo.vue';
+export default {
+    name: 'DashboardView',
+    components: {
+        OnsDoel,
+        Verbruik,
+        ExtraInfo
+    }
+}
+</script>
+
+<style scoped>
+    main {
+        display: flex;
+        justify-content: space-evenly;
+        align-items: center;
+        width: 100vw;
+        height: 88vh;
+    }
+</style>
