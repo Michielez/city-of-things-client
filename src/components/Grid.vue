9ba126572f5143f8cef78ea3e01ae364e3876b8f
Michiel Stragier <michiel.stragier@student.howest.be>
2023-12-20 10:36:22 +0100

    basic mapbox working

--- src/components/Grid.vue
+++ src/components/Grid.vue
@@ -1,11 +1,16 @@
 <script>
+import MapboxMap from '../components/MapboxMap.vue';
 export default {
   name: "Grid",
+  components:{
+    MapboxMap
+  }
 }
 </script>
 
 <template>
   <p>The grid</p>
+  <MapboxMap />
 </template>
 
 <style lang="scss">
---
+++ src/components/MapboxMap.vue
@@ -0,0 +1,41 @@
+<template>
+    <div ref="mapContainer" class="mapContainer"></div>
+</template>
+
+<script>
+
+import mapboxgl from 'mapbox-gl';
+import 'mapbox-gl/dist/mapbox-gl.css';
+
+import EnergyIcon from './EnergyIcon.vue';
+export default {
+    name: "MapboxMap",
+    components: {
+        EnergyIcon
+    },
+    data() {
+        return {
+            map: null
+        }
+    },
+    mounted(){
+        mapboxgl.accessToken = "pk.eyJ1IjoibWljaGllbC1zdHJhZ2llciIsImEiOiJjbHBzZnZpN3gwM2Q5Mmpxc2IwZzU1OHB0In0.XTKi-w4ZbMXY7Q2kQ6Jbig";
+
+        this.map = new mapboxgl.Map({
+            container: this.$refs.mapContainer,
+            style: 'mapbox://styles/mapbox/streets-v12',
+            center: [3.252,50.8256969], // starting position [lng, lat]
+            zoom: 15
+        })
+    }
+
+}
+</script>
+
+<style scoped>
+
+.mapContainer {
+  height: 60vh;
+  width: 60vw;
+  }
+</style>
