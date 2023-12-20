a30c0a7fcbe26b4ece18030578e77ab4e6ad83b9
Verdegem Tom <tom.verdegem@student.howest.be>
2023-12-20 15:40:37 +0100

    add groene cijfers responsive styling

--- src/components/GreenFacts.vue
+++ src/components/GreenFacts.vue
@@ -17,32 +17,74 @@ export default {
 </script>
 <style lang="scss" scoped>
 #green-facts {
+    height: 58%;
     display: flex;
     flex-direction: column;
     align-items: center;
     background-color: #E6E6F5;
     box-shadow: 0px 2px 4px rgba(0, 0, 0, 0.2);
     border-radius: 1rem;
-    padding: 2rem;
-    flex: 1;
+
+    padding: 5%;
+
+    @media (max-width: 768px) {
+        height: 45vh;
+
+        margin-bottom: 2%;
+
+        padding-bottom: 8%;
+        h2 {
+            font-size: 5vw !important;
+            font-weight: 600;
+            margin-top: 5% !important;
+        }
+
+        p {
+            font-size: 4vw !important;
+            font-weight: 600;
+            margin-top: 2% !important;
+        }
+
+        li {
+            font-size: 4vw !important;
+            font-weight: 600;
+
+        }
+
+        img {
+            width: 20% !important;
+            height: auto !important;
+        }
+    }
 }
 #green-facts img {
-    height: 80px;
+    height: 21%;
 }
 #green-facts h2 {
-    margin-top: 10px;
-    font-size: 2rem;
+    margin-top: 2vh;
+    font-size: 2vw !important;
     font-weight: 600;
+    margin-bottom: 3vh;
+}
+
+#green-facts p {
+    font-size: 1.5vw;
+
+    margin-bottom: 2vh;
 }
 #green-facts ul {
     text-align: center;
 }
 #green-facts ul li:nth-child(odd) {
     background-color: #A8A8F5;
-    padding: 1rem 0rem;
     border-radius: 1rem;
+    padding: 1.2rem;
+    font-weight: 600;
+    font-size: 1.4vw;
 }
 #green-facts ul li:nth-child(even) {
+    font-size: 1.4vw;
+
     margin-top: 0.5rem;
     margin-bottom: 0.5rem;
     font-weight: 600;
