073039af9a1183aaf4ec48e4f3380aa405aa68a1
Michiel Stragier <michiel.stragier@student.howest.be>
2024-01-04 01:03:03 +0100

    basic url fetching test

--- src/components/Grid.vue
+++ src/components/Grid.vue
@@ -1,16 +1,35 @@
 <script>
 import LeafletMap from '../components/LeafletMap.vue';
 import buildings from '../data/buildings'
+import { onMounted, ref } from 'vue'
+
 export default {
   name: "Grid",
-  data(){
-    return {
-      buildings: buildings
-    }
-  },
-
-  components:{
+  components: {
     LeafletMap  
+  },
+  setup() {
+    const buildingsData = ref(buildings);
+
+    onMounted(async () => {
+      try {
+        const url = "https://cms.cot.thiboverbeerst.cloud/api/buildings";
+        const response = await fetch(url);
+
+        if (response.ok) {
+          buildingsData.value = await response.json();
+          console.log(buildingsData.value);
+        } else {
+          console.error("Failed to fetch buildings:", response.status);
+        }
+      } catch (error) {
+        console.error("Error during fetch:", error);
+      }
+    });
+
+    return {
+      buildings: buildingsData
+    };
   }
 }
 </script>
@@ -18,12 +37,11 @@ export default {
 <template>
   <div class="container">
     <LeafletMap :buildings="buildings"/>
-
   </div>
 </template>
 
 <style lang="scss">
-  .container{
+  .container {
     width: 92%;
     margin: 0 auto;
     height: 82vh;
